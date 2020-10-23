3b68d63bcea5adea641e17dde832bab0d050c65d
vushu <danvu.hustle@gmail.com>
2020-10-23 23:59:45 +0200

    removed nanovg

--- bismuth/src/core/application.cpp
+++ bismuth/src/core/application.cpp
@@ -17,10 +17,10 @@
 #include <emscripten/emscripten.h>
 #endif
 //nanovg
-#include <nanovg/nanovg.h>
-#define NANOVG_GLES3_IMPLEMENTATION
-#include <nanovg/nanovg_gl.h>
-#include <nanovg/nanovg_gl_utils.h>
+//#include <nanovg/nanovg.h>
+//#define NANOVG_GLES3_IMPLEMENTATION
+//#include <nanovg/nanovg_gl.h>
+//#include <nanovg/nanovg_gl_utils.h>
 using namespace bi;
 
 Application::Application()
