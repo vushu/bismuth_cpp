d4941d62ea9b3d4f4931be01baad875bcf07be4c
Dan Vu <dhv@ajoursystem.dk>
2021-01-12 12:28:16 +0100

    updated

--- bismuth/include/bismuth/gui/guiwindow.hpp
+++ bismuth/include/bismuth/gui/guiwindow.hpp
@@ -1,3 +1,4 @@
+#include "bismuth/color.hpp"
 #include <bismuth/gui/guielement.hpp>
 namespace bi{
     class GuiWindow : public GuiElement {
@@ -9,11 +10,16 @@ namespace bi{
             void draw() override;
             void handleMouseClick(int action, glm::vec2 position) override;
             GuiWindow& setOutlineWidth(float outlineWidth);
+            GuiWindow& setBackgroundColor(glm::vec4 color);
+            GuiWindow& setOutlineColor(glm::vec4 color);
+            GuiWindow& setPosition(glm::vec2 position);
+            GuiWindow& setSize(glm::vec2 size);
 
         private:
             glm::vec2 position {0,0};
             glm::vec2 size {200,200};
             float outlineWidth = 4.0f;
-
+            glm::vec4 outlineColor = color::SOFT_BLUE;
+            glm::vec4 backgroundColor = color::WHITE;
     };
 }
--- bismuth/src/gui/guiwindow.cpp
+++ bismuth/src/gui/guiwindow.cpp
@@ -8,15 +8,36 @@ GuiWindow::GuiWindow() {
 
 }
 
+GuiWindow& GuiWindow::setSize(glm::vec2 size) {
+    this->size = size;
+    return *this;
+}
+
+GuiWindow& GuiWindow::setPosition(glm::vec2 position) {
+    this->position = position;
+    return *this;
+}
+
 GuiWindow& GuiWindow::setOutlineWidth(float outlineWidth) {
     this->outlineWidth = outlineWidth;
     return *this;
 }
+
+GuiWindow& GuiWindow::setBackgroundColor(glm::vec4 color)  {
+    this->backgroundColor = color;
+    return *this;
+}
+
+GuiWindow& GuiWindow::setOutlineColor(glm::vec4 color)  {
+    this->outlineColor = color;
+    return *this;
+}
+
 void GuiWindow::draw() {
     ioManager().shaperenderer->fill();
-    ioManager().shaperenderer->drawRect(this->position, this->size, bi::color::WHITE).endFlushBegin();
+    ioManager().shaperenderer->drawRect(this->position, this->size, backgroundColor).endFlushBegin();
     ioManager().shaperenderer->setLineWidth(this->outlineWidth);
-    ioManager().shaperenderer->drawRect(this->position, this->size, bi::color::SOFT_BLUE).endFlushBegin();
+    ioManager().shaperenderer->drawRect(this->position, this->size, outlineColor).endFlushBegin();
 }
 
 void GuiWindow::handleMouseClick(int action, glm::vec2 position) {
