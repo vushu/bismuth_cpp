bc33576258c2d6147c7fd790550505ac96ec3d23
Dan Vu <dhv@ajoursystem.dk>
2021-01-12 08:08:03 +0100

    fixed namespace

--- bismuth/src/gui/guiwindow.cpp
+++ bismuth/src/gui/guiwindow.cpp
@@ -1,12 +1,12 @@
 #include "bismuth/color.hpp"
 #include "bismuth/iomanager.hpp"
 #include <bismuth/gui/guiwindow.hpp>
-
+using namespace bi;
 void GuiWindow::draw() {
-    bi::ioManager().shaperenderer->drawRect(this->position, this->size, bi::color::WHITE).endFlushBegin(true);
-    bi::ioManager().shaperenderer->drawRect(this->position, this->size, bi::color::SOFT_BLUE).endFlushBegin();
+    ioManager().shaperenderer->drawRect(this->position, this->size, bi::color::WHITE).endFlushBegin(true);
+    ioManager().shaperenderer->drawRect(this->position, this->size, bi::color::SOFT_BLUE).endFlushBegin();
 }
 
 void GuiWindow::handleMouseClick(int action, glm::vec2 position) {
-    bi::log("handleMouseClick is not implemented yet");
+    log("handleMouseClick is not implemented yet");
 }
